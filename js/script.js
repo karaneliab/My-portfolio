eb835dee530e2ea931b403a6658299731d93358c
karaneliab <eliabkaran7884@gmail.com>
2023-12-11 09:53:16 +0300

    corrected

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 let button = document.getElementById("mimic")
 function gohome() {
-    window.location.href= "http://127.0.0.1:5500/Index.html"
+    window.location.href= "http://127.0.0.1:5500/index.html"
 }
